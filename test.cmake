ce22745620381e452ba260601ac893be5b96f106
Daniel Thornburgh <mysterymath@gmail.com>
2022-11-04 10:16:42 -0700

    Fill out test.

--- test.cmake
+++ test.cmake
@@ -1,1 +1,11 @@
-find_program(SIM mos-sim REQUIRED)
+execute_process(COMMAND ${SIM} ${HELLO}
+   OUTPUT_FILE output RESULT_VARIABLE RESULT)
+if (RESULT)
+  message(FATAL_ERROR "Error running simulated test.")
+endif()
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E compare_files --ignore-eol output ${EXPECTED}
+   RESULT_VARIABLE RESULT)
+if (RESULT)
+  message(FATAL_ERROR "Incorrect output.")
+endif()
